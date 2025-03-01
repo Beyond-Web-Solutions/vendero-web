b1d949d625ae8bcfbe17e919de751837763da357
MeesEgberts <mees.egberts@beyond-web.nl>
2025-03-01 16:34:10 +0100

    remove unused sign-in form imports and initialization

--- src/app/[locale]/(auth)/sign-in/_components/form.tsx
+++ src/app/[locale]/(auth)/sign-in/_components/form.tsx
@@ -1,17 +1,5 @@
 "use client";
 
-import { useForm } from "react-hook-form";
-import {
-  SignInFormData,
-  signInSchema,
-} from "@vendero/app/[locale]/(auth)/sign-in/_lib/definitions/sign-in";
-import { zodResolver } from "@hookform/resolvers/zod";
-
 export function SignInForm() {
-  const form = useForm<SignInFormData>({
-    resolver: zodResolver(signInSchema),
-    defaultValues: {},
-  });
-
   return null;
 }
